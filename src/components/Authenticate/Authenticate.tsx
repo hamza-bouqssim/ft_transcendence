1c11edcde915495e045fed7e4158b9f0f0884a86
yassine-khadiri <yassinekhadiri77@gmail.com>
2023-07-19 19:28:10 +0100

    Add Some Effets To Form Validation

--- src/components/Authenticate/Authenticate.tsx
+++ src/components/Authenticate/Authenticate.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { forwardRef, useRef } from "react";
 import Image from "next/image";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import "./Authenticate.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faKey, faEnvelope } from "@fortawesome/free-solid-svg-icons";
@@ -99,6 +99,9 @@ const Authenticate = () => {
 // Sign In Component :)
 const SignIn = forwardRef( (props: any, ref: any) => {
 
+	// const emailInputRef = useRef<HTMLInputElement>(null),
+	// 	passInputRef = useRef<HTMLInputElement>(null);
+
 	type FormData = {
 		email: string;
 		password: string;
@@ -106,31 +109,55 @@ const SignIn = forwardRef( (props: any, ref: any) => {
 
 	const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
-	// const onSubmit = () => {
-	// 	if (errors.email?)
-	// };
+	const onSubmit: SubmitHandler<FormData> = (data) =>  {
+		// console.log(data)
+
+		const emailInput = ref.current?.querySelector('input[type="email"]'),
+				passInput = ref.current?.querySelector('input[type="password"]');
+
+		// console.log("email:" + data.email);
+		// console.log("password " + data.password);
+		if (!data.email.length)
+		{
+			emailInput.parentNode.classList.add("animate-wiggle");
+			emailInput.style.cssText = "border: 3px solid red";
+			emailInput.parentNode.addEventListener( 'animationend', () => {
+				emailInput.parentNode.classList.remove("animate-wiggle");
+				emailInput.style.removeProperty("border");
+			} );
+		}
+		if (!data.password.length)
+		{
+			passInput.parentNode.classList.add("animate-wiggle");
+			passInput.style.cssText = "border: 3px solid red";
+			passInput.parentNode.addEventListener( 'animationend', () => {
+				passInput.parentNode.classList.remove("animate-wiggle");
+				passInput.style.removeProperty("border");
+			} );
+		}
+	};
+
 // console.log(errors);
 	return (
 		<div ref={ref}>
 			<form
 				action=""
 				className="relative h-full w-full flex items-center justify-center flex-col gap-3 sm:gap-4"
-				onSubmit={handleSubmit( (data) => {
-					console.log(data); } )}
-				// onSubmit={onSubmit}
-					>
+				onSubmit={handleSubmit(onSubmit)} >
 				<div className="relative">
 					<FontAwesomeIcon icon={faEnvelope} className="absolute top-[50%] left-[10%] transform translate-y-[-50%] text-black text-[.7rem] md:text-sm" />
 					<input
 						type="email"
 						className="custom-shape input-style" placeholder="email"
-						{...register("email", {required: "true"}) } />
+						{...register("email") } />
 				</div>
 				<div className="relative">
 					<FontAwesomeIcon icon={faKey} className="absolute top-[50%] left-[10%] transform translate-y-[-50%] text-black text-[.7rem] md:text-sm" />
-					<input type="password"
+					<input
+						// ref={passInputRef}
+						type="password"
 						className="custom-shape input-style" placeholder="password"
-						{...register("password", {required: true, minLength: 8})}  />
+						{...register("password")}  />
 				</div>
 				<button type="submit" className="custom-shape btn-style text-[.7rem] bg-[var(--purple-color)] transition ease-in-out duration-700 sm:text-sm md:text-[1rem] md:py-4 md:w-[166px]" >
 					Sign In
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        wiggle: 'wiggle 1.2s linear',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 7%': { transform: 'rotateZ(0)' },
+          '15%': { transform: 'rotateZ(-15deg)' },
+          '20%': { transform: 'rotateZ(10deg)' },
+          '25%': { transform: 'rotateZ(-10deg)' },
+          '30%': { transform: 'rotateZ(6deg)' },
+          '35%': { transform: 'rotateZ(-4deg)' },
+          '40%, 100%': { transform: 'rotateZ(0)' },
+        }
+      },
+      // keyframes: {
+      //   wiggle: {
+      //     '0%, 100%': { transform: 'rotate(-3deg)' },
+      //     '50%': { transform: 'rotate(3deg)' },
+      //   }
+      // },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
