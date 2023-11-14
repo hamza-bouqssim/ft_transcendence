7aaa5b1ecb4864925df21cf4a6132e5f98aea78c
rgatnaou <gatnaouiradwane@gmail.com>
2023-11-14 10:16:28 +0100

    fix seed.ts

--- backend/src/game/game.controller.ts
+++ backend/src/game/game.controller.ts
@@ -1,10 +1,34 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post , Get , Delete ,Body} from '@nestjs/common';
+import { GameService } from './game.service';
+import { UserService } from 'src/user/user.service';
 
 @Controller('game')
 export class GameController {
-    @Post()
-    creatGame()
+    constructor(private readonly gameService: GameService) {}
+    @Get()
+    test()
     {
+        return "test";
+    }
+    
+    @Post('join')
+    async joinQueue(@Body('id') id :  string)
+    {
+        console.log("id",id);
+       return await this.gameService.joinQueue(id);
+    }
 
+    @Delete('leave')
+    async leaveQueue(@Body('id') id: string)
+    {
+        await this.gameService.leaveQueue(id);
+    }
+
+    @Get('queue')
+    async getQueue()
+    {
+        console.log("get queue");
+        return "get queue";
+        // await this.gameService.getQueue();
     }
 }
--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -5,36 +5,76 @@ import { PrismaService } from 'prisma/prisma.service';
 export class GameService {
     constructor(private readonly prisma: PrismaService) {}
 
-    async readyToPlay(user: any)
+    async readyToPlay()
     {
         const queueCount = await this.prisma.queue.count()
         if (queueCount >= 2)
         {
+            const  room = await this.prisma.queue.findMany({
+                orderBy: {
+                  createdAt: 'asc'
+                },
+                take: 2,
+                include: {
+                  user: true
+                }
+              });
+            console.log(room);
             console.log("ready to play");
+            
         }
     }
 
-    async joinQueue(user: any)
+    async findUserById(id: string)
+    {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: id
+            }
+        });
+        return user;
+    }
+
+    async joinQueue(userId: string)
     {
         const join = await this.prisma.queue.create({
             data: {
+                
                 user: {
                     connect: {
-                        id: user.id
-                    }
-                }
-            }
-        });
+                        id: userId,
+                        },
+                    },
+                },
+        })
+        console.log(join)
+        // this.readyToPlay();
         return join;
     }
+    
+    async leaveQueue(id: string)
+    {
+        const leave = await this.prisma.queue.delete({
+            where: {
+                userId: id
+            }
+        });
+        return leave;
+    }
 
-    // async leaveQueue(user: any)
-    // {
-    //     const leave = await this.prisma.queue.delete({
-    //         where: {
-    //             userId: user.id
-    //         }
-    //     });
-    //     return leave;
-    // }
+    async getQueue()
+    {
+        const rooms = await this.prisma.queue.findMany({
+            include: {
+                user: true
+            }
+        });
+
+        console.log("get rooms : ");
+        if (rooms.length > 0)
+        {
+            console.log(rooms);
+            return rooms;
+        }
+    }
 }
