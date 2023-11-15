4431b08985ac6b105b4ea3a964407da3f11353ef
rgatnaou <gatnaouiradwane@gmail.com>
2023-11-15 17:50:03 +0100

    inprogress : logic achievement

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -106,11 +106,14 @@ export class GameService {
         return match;
     }
     
-    async deletePongMatch(id: string)
+    async deletePongMatch(playerId: string)
     {
-        const match = await this.prisma.pong_match.delete({
+        const match = await this.prisma.pong_match.deleteMany({
             where: {
-                id: id
+                OR: [
+                    { player1_id: playerId },
+                    { player2_id: playerId },
+                ],
             }
         });
         return match;
@@ -132,8 +135,7 @@ export class GameService {
 
     // part of match_history
     async createMatchHistory(userIdOne : string, userIdTwo : string,
-                            playerOne_score : number, playerTwo_score : number, matchId : string)
-    {
+                            playerOne_score : number, playerTwo_score : number, matchId : string) {
         const match = await this.prisma.match_History.create({
             data: {
                 playerone: { connect: { id: userIdOne } },
@@ -146,4 +148,155 @@ export class GameService {
         console.log(match)
         return match;
     }
-}
+    async deleteMatchHistory(playerId: string)
+    {
+        const deleteMatch = await this.prisma.match_History.deleteMany({
+            where : {
+                OR: [
+                    { playerOne: playerId },
+                    { playerTwo: playerId },
+                ],
+            }
+        });
+        this.deletePongMatch(playerId);
+        return deleteMatch;
+    }
+
+    async getMatchHistory(userId: string){
+        const matchHistory = await this.prisma.match_History.findMany({
+            where: {
+                OR: [
+                    { playerOne: userId },
+                    { playerTwo: userId },
+                ],
+            }
+        });
+        return matchHistory;
+    }
+
+    // part of StateGame
+    level(win : number): number {
+        const currentLevel = win * (100 * 0.05) - (win * 0.05);
+        // const increas = (100 - currentLevel) * 0.05;
+        // const updateLevel : number  = currentLevel + increas;
+        return currentLevel;
+    }
+
+    async rank(level: number,userId : string){
+            const users = await this.prisma.stateGame.findMany({
+                select: {
+                    level: true,
+                    user_id: true,
+                },
+            });
+        
+            // Sort users by level in descending order
+            const sortedUsers = users.sort((a, b) => b.level - a.level);
+        
+            // Find the index of the user with the specified userId
+            const userIndex = sortedUsers.findIndex((user) => user.user_id === userId);
+        
+            // Return the rank (1-based) or 0 if the user is not found
+            return userIndex !== -1 ? userIndex + 1 : 0;
+        
+    }
+
+    async updateStateGame(win : number,lose: number,numberOfMatch: number, userId: string) {
+        const level = this.level(win);
+        const rank = await this.rank(level,userId);
+        const state = await this.prisma.stateGame.update({
+            where : {
+                user_id: userId
+            },
+            data: {
+                win,
+                lose,
+                numberOfMatch,
+                level: level,
+                rank: rank,
+                user: { connect: { id : userId},}
+            },
+        })
+    }
+
+    async createStateGame(userId: string) {
+        const state = await this.prisma.stateGame.create({
+            data: {
+                win: 0,
+                lose: 0,
+                numberOfMatch: 0,
+                level: 0,
+                rank: 0,
+                user: { connect: { id : userId},}
+            },
+        })
+    }
+
+    async deleteStateGame(userId: string){
+        const deleteState = await this.prisma.stateGame.delete({
+            where : {
+                user_id: userId
+            }
+        });
+        return deleteState;
+    }
+
+    async getStateGame(userId: string){
+        const state = await this.prisma.stateGame.findMany({
+            where: {
+                user_id: userId
+            }
+        });
+        return state;
+    }
+    // part of Achievement
+    async getAchievement(userId: string){
+        const achievements = await this.prisma.achievement.findMany({
+            where: {
+                user_id: userId
+            }
+        });
+        return achievements;
+    }
+
+    async createAchievement(userId: string) {
+        const achievement = await this.prisma.achievement.create({
+            data: {
+                user: { connect: { id : userId},},
+                content: 'Welcome to Pong Game',
+                type   : 'Welcome',
+                picture: '',
+                win  : 0,
+                lose : 0,
+                level : 0,
+            },
+        })
+    }
+
+    async deleteAchievements(userId: string){
+        const deleteAchievement = await this.prisma.achievement.deleteMany({
+            where : {
+                user_id: userId
+            }
+        });
+        return deleteAchievement;
+    }
+
+    async updateAchievement(win : number,lose: number,level: number, userId: string) {
+        const achievement = await this.prisma.achievement.update({
+            where : {
+                user_id: userId
+            },
+            data: {
+                win,
+                lose,
+                level,
+                user: { connect: { id : userId},}
+            },
+        })
+    }
+
+}   
+
+
+
