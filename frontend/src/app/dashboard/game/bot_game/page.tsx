ea9b44ce8356073ebfa7557978d0cb62f8afb046
yassine-khadiri <yassinekhadiri77@gmail.com>
2023-11-17 19:14:39 +0100

    Working On Making Game Responsive

--- frontend/src/app/dashboard/game/bot_game/page.tsx
+++ frontend/src/app/dashboard/game/bot_game/page.tsx
@@ -5,10 +5,6 @@ const Game = () => {
 	return (
 		<section className="relative h-screen min-h-[653px] text-white md:min-h-[900px] xl:min-h-[800px]">
 			<BotGame />
-			{/* <div className="absolute left-[50%] top-[50%] flex min-h-[700px] w-full max-w-[1600px] -translate-x-[50%] -translate-y-[50%] flex-col items-center justify-center px-4 py-8 lg:flex-row lg:items-end lg:justify-evenly min-[1750px]:h-[900px]"> */}
-			{/* <div className="absolute left-[50%] top-[50%] flex min-h-[700px] w-full max-w-[1600px] -translate-x-[50%] -translate-y-[50%] flex-col items-center justify-center px-4 py-8 lg:flex-row lg:items-end lg:justify-evenly min-[1750px]:h-[900px]">
-				<BotGame />
-			</div> */}
 		</section>
 	);
 };
