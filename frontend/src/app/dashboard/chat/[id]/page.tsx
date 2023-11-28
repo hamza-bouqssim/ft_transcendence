41f185b2caa54e7ba717e58094852d7275cffd48
jallolmohamed <medjalal1997@gmmail.com>
2023-11-28 09:01:15 -0500

    d

--- frontend/src/app/dashboard/chat/[id]/page.tsx
+++ frontend/src/app/dashboard/chat/[id]/page.tsx
@@ -1,113 +1,66 @@
 "use client"
+import {  useEffect, useState } from "react"
+import {useRouter} from "next/navigation"
+import { usePathname } from 'next/navigation'
+import ConversationSearch from "../ConversationSearch/page"
+import ChatComponnent from "../ChatComponent/ChatComponent"
+import GroupsManagement from "../GroupsManagment/GroupsManagment"
+import CreatGroups from "../CreateGroups/CreateGroups"
+import {IoMdAdd} from 'react-icons/io'
+import OnlineFriends from "../OnlineFriends/OnlineFriends"
+import ListFriends from "../ListFriends/ListFriends"
+import SendRequest from "../SendRequest/SendRequest"
+import FriendsBloque from "../FriendsBloque/FriendsBloque"
 
-import CoversationSideBar from "@/app/components/CoversationSideBar/ConversationSideBar";
-import { ConversationChannelStyle, Page} from "@/app/utils/styles";
-import { useContext, useEffect, useState , PropsWithChildren} from "react";
-import { ConversationTypes, User, UsersTypes, messageEventPayload, messageTypes } from "@/app/utils/types";
-import { getAuthUser, getConversation, getConversationMessage } from "@/app/utils/api";
-import { useParams } from "next/navigation";
-import MessagePanel from "@/app/components/messages/MessagePanel";
-import TopRightBar from "@/app/components/TopRightBar";
-import SideBar from "@/app/components/SideBar";
-import { socketContext } from "@/app/utils/context/socketContext";
-import { AppDispatch, RootState, store } from "@/app/store";
-import {Provider as ReduxProvider, useDispatch, useSelector} from 'react-redux'
-import { fetchMessagesThunk } from "@/app/store/messageSlice";
-import { fetchConversationThunk } from "@/app/store/conversationSlice";
-import { io } from "socket.io-client";
+const CoversationSideBar = () => {
+	const [newRooms , setNewRooms]  = useState<boolean>(false)
+	const router = useRouter();
+	const pathname = usePathname()
+ 	const [selectUsers, setSelectUsers] = useState<string>("Chat");     
 
-
-
-const ConversationChannelPage = () => {
-  const socket = io("http://localhost:8000", {
-    withCredentials: true,
-});
-    const [ user, setUser] = useState<User | undefined>();
-    const [loading, setLoading] = useState<boolean>(false);
-    useEffect(() => {
-            setLoading(true);
-            getAuthUser().then(({data}) => {
-                setUser(data);
-                setLoading(false)})
-            .catch((err)=> { setLoading(false);});
-    },[])
-	const [conversation , setConversation] = useState<ConversationTypes[]>([]);
-  const dispatch = useDispatch<AppDispatch>();
- 
-
-
-    const {id} = useParams();
-    const [message , setMessage] = useState<messageTypes[]>([])
-
-   
-      const conversations = useSelector(
-        (state: RootState) => state.conversation.conversations
-      );
-    
-      useEffect(() => {
-        dispatch(fetchConversationThunk());
-      }, []);
-    
-      useEffect (() => {
-        const conversationId = id;
-        dispatch(fetchMessagesThunk(conversationId))
-        .unwrap()
-        .then(({data}) => {
-          setMessage(data);
-        }).catch((err)=>{
-        }
-        );
-      },[])
-
-      // for sockets
-
-      //whenever we enter to channel page we subscribe to connected and onMessage
-      useEffect(()=>{
-        socket.emit('onClientConnect', {conversationId : id});
-        socket.on('connected', ()=> {
-        })
-          // socket.on('connected', () => console.log("socket here connected"));
-          //iam listenning to socket-io on onMessage and whenever an message receive we bussically update the state with setMessage
-          socket.on('onMessage', (payload : messageEventPayload) => {
-            const {conversation} = payload;
-            setMessage((prev) => [payload, ...prev]);
-          });
-          return () =>{
-            socket.off('connected');
-            socket.off('onMessage');
-          }
-      }, [])
-      const sendTypingStatus = () =>{
-        socket.emit('onUserTyping', {conversationId : id})
-      }
-	    const [onlineUsers, setOnlineUsers] = useState<UsersTypes[]>([]);
-
-
-      useEffect(() => {
-        socket.on('updateOnlineUsers', (onlineUsers) => {
-          setOnlineUsers(onlineUsers);
-        });
-      
-        return () => {
-          socket.off('updateOnlineUsers');
-        };
-      }, [socket]);
-    return ( 
-
-            <div className=" flex h-screen  xl:container xl:mx-auto">
-              <div className ="hidden xl:block h-full w-[35%] p-10 pl-5 pr-2 ">
-                <CoversationSideBar onlineUsers={onlineUsers}/>
-              </div>
-                <div className="bg-white xl:m-10  xl:mr-10 xl:ml-2 w-full xl:w-[65%]  xl:rounded-[20px]">
-                    <MessagePanel messages={message} ></MessagePanel> 
-                </div>
-            </div>
-
-
-            
-      
-        
+    return (
+		<div className="w-full h-full relative p-2  xl:rounded-[20px] pt-4 bg-white">
+			{/* {show &&  <CreateConversationModal   setShow={setShow} />   } */}
+			<div className="flex items-center rounded-full justify-between w-3/4 mb-4  mx-auto  bg-[#DFDFDF]">
+				<button
+					onClick={()=>{router.push("/dashboard/chat")}}
+					className={`${(pathname.includes('chat')) ? 'bg-[#5B8CD3]' : ''} p-4 rounded-full w-1/2`}>Chats
+				</button>
+				<button 
+					onClick={()=>{router.push("/dashboard/groups")}}
+					className={`${(pathname.includes('groups')) ? 'bg-[#5B8CD3]' : ''} rounded-full p-4 w-1/2`}>Group
+				</button>
+			</div>
+			<hr className="bg-[#DFDFDF] w-1/2 mx-auto mt-5"/>
+			{!newRooms && <ConversationSearch  ></ConversationSearch>}
+			{newRooms && pathname.includes('chat')  && <div className="flex items-center rounded-full justify-between w-3/ mb-4  mx-auto  bg-[#423d3d]">
+ 				 <button
+ 					onClick={()=>{setSelectUsers("Chat")}} className={`${(selectUsers ==='Chat') ? 'bg-[#5B8CD3]' : ''} p-4 rounded-full w-1/2`}>Chat
+ 				</button>
+ 				<button 
+ 					onClick={()=>{setSelectUsers("online")}}
+ 					className={`${(selectUsers ==='online') ? 'bg-[#5B8CD3]' : ''} p-4 rounded-full w-1/2`}>Online
+ 				</button>
+ 				<button
+ 					onClick={()=>{setSelectUsers("allFriends")}} className={`${(selectUsers ==='allFriends') ? 'bg-[#5B8CD3]' : ''} p-4 rounded-full w-1/2`}>Friends
+ 				</button>
+ 				<button
+ 					onClick={()=>{setSelectUsers("EnAttent")}} className={`${(selectUsers ==='EnAttent') ? 'bg-[#5B8CD3]' : ''} p-4 rounded-full w-1/2`}>Pending
+ 				</button>
+ 				<button
+ 					onClick={()=>{setSelectUsers("Bloques")}} className={`${(selectUsers ==='Bloques') ? 'bg-[#5B8CD3]' : ''} p-4 rounded-full w-1/2`}>Bloques
+ 				</button>
+ 			</div>}
+			{newRooms &&  pathname.includes('chat')  && selectUsers === 'online'  ? <OnlineFriends  /> :  (pathname.includes('chat') && selectUsers === 'allFriends') ? <ListFriends/> : ( pathname.includes('chat') && selectUsers === 'EnAttent') ? <SendRequest/> :( pathname.includes('chat') && selectUsers === 'Bloques') && <FriendsBloque/> }
+			{!newRooms &&  pathname.includes('chat') && <ChatComponnent/>}
+			{!newRooms &&  pathname.includes('groups') && <GroupsManagement/>}
+			{newRooms && (pathname.includes('chat')? <div className="text-black">Addchat</div> : <CreatGroups setNewRooms={setNewRooms} ></CreatGroups> )}
+			{!newRooms  &&
+				<button onClick={()=>{setNewRooms(true)}} className="absolute right-5 p-4 bottom-20 md:bottom-4 bg-[#5B8CD3] rounded-full "><IoMdAdd />
+				</button>
+			}
+		</div>
      );
 }
  
-export default ConversationChannelPage;
+export default CoversationSideBar;
