480050a5754fa28797bf7f2e889463225de48077
hamza-bouqssim <hamzaboukssim6@gmail.com>
2023-12-16 01:03:04 +0100

    just adding some styling

--- frontend/src/app/components/HistoryMatches.tsx
+++ frontend/src/app/components/HistoryMatches.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HistoryMatches = ({playerOne, resultOne, playerTwo, resultTwo, duration, date, totalMatches}) => {
   return (
-    <div className="history-results mt-[20px]  w-full h-[40px] bg-[#79a9f28d] rounded-[40px] flex  justify-between   px-1 py-1 hover:bg-[#95c0ff8d]">
+    <div className="history-results mt-[20px]  w-full h-[40px] bg-[#79a9f28d] shadow-lg rounded-[40px] flex  justify-between   px-1 py-1 hover:bg-[#95c0ff8d]">
 
         <div className='res w-[23%]  text-center flex justify-between'>
           <img src={playerOne} alt="" className='rounded-full' />
