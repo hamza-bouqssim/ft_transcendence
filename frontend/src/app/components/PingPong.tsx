477cf07642917e365e65f604bc3c969a5d34604f
yassine-khadiri <yassinekhadiri77@gmail.com>
2023-12-27 16:57:57 +0100

    create history match

--- frontend/src/app/components/PingPong.tsx
+++ frontend/src/app/components/PingPong.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
